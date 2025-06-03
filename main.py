82d9f5308af3b54ad8abc078b32043e5a5560a33
cpettifo <cpettifo@myune.edu.au>
2025-06-03 18:51:45 +0200

    add synopsis

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# The goal of this script is to allow for the analysis of structured HEI data to allow for insertion into the 
+# World Higher Education Database, the first iteration uses CRICOS but given time I plan to modify this to take
+# any structured data as input
+
+
 import categorise # type: ignore
 
 masterlist_path = "masterlist.xlsx"
