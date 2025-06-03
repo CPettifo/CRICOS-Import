3bb42f2bbd27dcad97f4255b30ad0e1feecbcc99
CPettifo <craig_petti4@hotmail.com>
2025-06-03 14:33:05 +0200

    continue psuedocode

--- categorise.py
+++ categorise.py
@@ -6,40 +6,71 @@
 
 # Before the process begins the latest list of WHED-recognised institutions in Australia is exported with their ID and added as a sheet in the masterlist
 
+def main():
+        
 
-# Read masterlist
+    ###Initialise Variables###
+    # WHED-Recognised Institutions not recognised by CRICOS
+    whed_check = 0
 
+    # WHED-Candidates within CRICOS
+    whed_candidates = 0
 
+    # Non WHED-Level Institutions
+    whed_excluded = 0
 
-# Categorise CRICOS institutions
-# For each institution
+    ###Initialise Lists###
 
+    # List of Level Codes that are considered post-grad by the WHED
+    postgrad_codes = ["6C", "7A", "7B", "7C", "7D"]
 
-    #TODO Create a function that will take as input an institution name, list of credentials for each category
-
-
-    # Offers <= Bachelor Degrees, these are Not WHED-level
-
-
-    # Offers >= Bachelor Honours, these are WHED candidates (will have to check whether they have a certain number of graduate cohorts)
-
-
-    #TODO Create function that takes as input the institution name
-    # Same as above but name matches WHED-Recognised institutions
-
-
-    # Institutions that are WHED-Recognised that do not appear in the CRICOS list will be given a "To Check" value
+    # List of credential titles from the spreadsheet (this could later be turned into a query by )
+    postgrad_list = get_postgrad_list()
 
 
+    # Read masterlist
 
 
 
-#TODO Identify relevant CRICOS columns
+
+    # For each institution
+        # Check that they are considered by CRICOS as Active
+
+        #TODO Create a function that will take as input an institution name, list of credentials for each category
+
+        # Offers >= Bachelor Honours, these are WHED candidates (will have to check whether they have a certain number of graduate cohorts)
+
+
+        # Else whed_excluded
+
+        #TODO Create function that takes as input the institution name
+        # Same as above but name matches WHED-Recognised institutions
+
+        # Add column for excluded categories, 
+        # in CRICOS but not WHED-level (i.e. doesn't offer Honours+)
+        # Not in CRICOS or name mismatch
 
 
 
-#TODO Identify target schema
+#TODO Flesh out this function
+# Checks the credentials offered at a specific institution to see if it is a possible WHED candidate
+def whed_check():
+    return 0
 
 
+# Prints a summary of the categorisation process in the terminal
+def output_summary():
+    return 0
 
-# 
+
+# Takes the list of postgrad codes as input and compares all credentials in the whed_levels sheet
+def get_postgrad_list(postgrad_codes):
+    postgrad_list = []
+    # open masterlist
+
+    # for row of credential name
+
+        # if column 2 in postgrad_codes:
+            #postgrad_list.append(name of cred)
+
+    return postgrad_list
---
+++ main.py
@@ -0,0 +1,8 @@
+import categorise # type: ignore
+
+# Ask for user input if categorisation is required
+
+
+# Categorise institutions (WHED-Recognised, WHED-Candidate, etc.)
+categorise.main()
+
