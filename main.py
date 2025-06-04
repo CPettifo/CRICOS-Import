e0204edb9531de469297862a0947f81d4533b3d8
CPettifo <cpettifo@myune.edu.au>
2025-06-04 07:31:00 +0200

    add temporary readme

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # The goal of this script is to allow for the analysis of structured HEI data to allow for insertion into the 
 # World Higher Education Database, the first iteration uses CRICOS but given time I plan to modify this to take
 # any structured data as input
+#TODO put this into the README too
+# The structured data is outlined in the README but I've put it here for myself also
 
+# there should be three spreadsheets in your workbook
+# ext_inst is a list of the institutions from the recognised government or credential recognition body
+# It should have the following columns in this order: institution ID, institution name, institution alternative name, institution homepage,
+# a concatenation of the institutional address
+# 
+# ext_cred is a list of the credentials offered 
+# It should have the following columns in this order: institution ID, institution name, whether credential is active (manually set this to "Yes" if it is not present in extraction)
+# Credential level (i.e. Bachelors, Masters, or NQF level), Course Name, FOS Levels if available and in descending order of hierarchy
 
 import categorise # type: ignore
 
@@ -18,7 +28,7 @@ if user_input == "Y":
     
     # list of level codes that categorise a degree as postgrad in the WHED
     postgrad_codes = ["6C", "7A", "7B", "7C", "7D"]
-    
+
     # Categorise institutions (WHED-Recognised, WHED-Candidate, etc.)
     categorise.main(glossary_path, masterlist_path, postgrad_codes)
 
