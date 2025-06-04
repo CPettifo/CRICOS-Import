067d521c81f1d6e20af2581f06a569b5192be759
CPettifo <cpettifo@myune.edu.au>
2025-06-04 07:34:46 +0200

    reverse logic of expired

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 # a concatenation of the institutional address
 # 
 # ext_cred is a list of the credentials offered 
-# It should have the following columns in this order: institution ID, institution name, whether credential is active (manually set this to "Yes" if it is not present in extraction)
+# It should have the following columns in this order: institution ID, institution name, whether credential is not expired (manually set this to "No" if it is not present in extraction)
 # Credential level (i.e. Bachelors, Masters, or NQF level), Course Name, FOS Levels if available and in descending order of hierarchy
 
 import categorise # type: ignore
